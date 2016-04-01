1fc85068edbdb858667122f8b0e5c1e9ca764026
Kristen Kanekuni <kxk9106@g.rit.edu>
2016-03-31 20:53:26 -0400

    Added Progress Bar

---
+++ Assets/Scripts/EarthQuake.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class EarthQuake : MonoBehaviour {
+
+	public Image earthQuake;
+	float charge = 2.0f;
+	public Text ready;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		earthQuake.fillAmount += 1.0f / charge * Time.deltaTime;
+		if (earthQuake.fillAmount >= 1) {
+			ready.text = "Ready";
+		}
+	
+	}
+}
