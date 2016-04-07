72c4cd13e580e51358e088cffc0ad6c402e74f8f
ToddWilliams459 <tkw8341@rit.edu>
2016-04-06 20:51:28 -0400

    Castle

---
+++ Assets/Scripts/Castle.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Castle : MonoBehaviour {
+
+	public GameObject tower;
+	public Transform target;
+	public float speed = 1f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//if (Input.GetKeyDown (KeyCode.Space)) {
+			float step = speed * Time.deltaTime;
+			tower.transform.position = Vector3.MoveTowards(tower.transform.position, target.position, step);
+
+
+		//}
+	}
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
+	public GameObject spawn;
+	public GameObject enemy;
+
+	Quaternion rot = Quaternion.Euler(0,0,0);
+
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			GameObject.Instantiate (enemy, spawn.transform.position, rot);
+		}
 	}
 }
