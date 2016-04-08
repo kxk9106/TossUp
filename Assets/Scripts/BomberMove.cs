1a13e3d7f938709c8b13ca485f6a618209dbea4f
Kristen Kanekuni <kxk9106@g.rit.edu>
2016-04-08 14:45:05 -0400

    Blood
    not right yet

--- Assets/Scripts/BomberMove.cs
+++ Assets/Scripts/BomberMove.cs
@@ -6,26 +6,43 @@ public class BomberMove : MonoBehaviour {
 	public float speed = 1;
 	public bool movingLeft = true;
 
+	SwipingScript swipeScript;
+
+	Sprite blood;
+
+	Vector3 temp;
+	bool dead = false;
+
+
+
 
 	// Use this for initialization
 	void Start () {
-		
+		swipeScript = FindObjectOfType (typeof(SwipingScript)) as SwipingScript;
+		blood = Resources.Load ("blood", typeof(Sprite)) as Sprite;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (movingLeft) {
+		if (movingLeft && dead == false) {
 			transform.position += Vector3.left * speed * Time.deltaTime;
 			if (transform.position.x < -2) {
 				movingLeft = false;
 			}
 			
 		} 
-		else {
+		else if (movingLeft == false && dead == false){
 			transform.position += Vector3.right * speed * Time.deltaTime;
 			if(transform.position.x > 6){
 				movingLeft = true;
 			}
+		}
+
+		if (transform.position.y <= -1 && dead == false) {
+			gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load ("blood", typeof(Sprite)) as Sprite;
+			temp = new Vector3(0,0,0);
+			transform.position = temp;
+			dead = true;
 		}
 		Debug.Log ("Transform" + transform.position.x);
 	}
