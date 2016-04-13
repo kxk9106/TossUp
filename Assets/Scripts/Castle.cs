5658450300db3fa21004f3579b50c072fc57efb2
ToddWilliams459 <tkw8341@rit.edu>
2016-04-13 03:06:50 -0400

    Splash Screen
    I created a splash screen. Lost track of my code when Kristen applied
    the Take Damage in the player thing so i think everything i wrote prior
    is obsolete.

--- Assets/Scripts/Castle.cs
+++ Assets/Scripts/Castle.cs
@@ -6,7 +6,6 @@ public class Castle : MonoBehaviour {
 	public GameObject tower;
 	public Transform target;
 	public float speed = 1f;
-	 float segment = 2f;
 	// Use this for initialization
 	void Start () {
 	
@@ -14,32 +13,29 @@ public class Castle : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		//if (Input.GetKeyDown (KeyCode.Space)) {
+		if (Input.GetKeyDown (KeyCode.Space)) {
 		//float step = speed * Time.deltaTime;
 		//tower.transform.position = Vector3.MoveTowards(tower.transform.position, target.position, step);
 
 		//takeDamage();
-		//}
+		}
 		//takeDamage ();
 	}
 
 	public void takeDamage(){
 
-		/*
+
 
 		//while (segment < .3839993f) {	
-		segment -= Time.deltaTime;
-		if (segment > -1.7) {
-			tower.transform.position -= new Vector3 (0, .5f, 0) * Time.deltaTime;
-		}
+			tower.transform.position -= new Vector3 (0, 20f, 0) * Time.deltaTime;
 			//segment += .001f * Time.deltaTime;
 		//}
 		//segment = 0f;
 
-		*/
-		if (tower.transform.position.y >= -0.5f) {
-			tower.transform.position -= new Vector3 (0, .5f, 0); 
-		}
+
+		//if (tower.transform.position.y >= -0.5f) {
+		//	tower.transform.position -= new Vector3 (0, .5f, 0); 
+		//}
 
 
 	}
