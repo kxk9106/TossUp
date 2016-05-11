a5341a18345211cafc6f226bf6b6df9bcf740382
Danny Lee <dxl1720@rit.edu>
2016-05-10 22:13:52 -0400

    Reorganized GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,42 +5,52 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
-	public GameObject spawn;
-	public GameObject enemy;
+	// vars to be set in unity
+	public GameObject walkerSpawn;
+	public GameObject bomberSpawn;
+	public GameObject walkerPrefab;
+	public GameObject bomberPrefab;
 
+	// vars that shouldnt be set in Unity, but accessed by other scripts
 	public List<GameObject> walkers;
-
-	Quaternion rot = Quaternion.Euler(0,0,0);
-
+	public List<GameObject> bombers;
+	public float totalGameScore;
+	public Text scoreText;
+	
 	const int FIRST_WAVE_COUNT = 10;
 
+	private Quaternion rot = Quaternion.Euler(0,0,0);
 	private bool waitForWave = true;
 	private float interWaveTimer;
-	private float intraSpawnTimer;
+	private float intraWalkerSpawnTimer;
+	private float intraBomberSpawnTimer;
 	private List<int> waveSpawnCount;
 	private int spawnCountLimit = FIRST_WAVE_COUNT; // First wave count
 	private int currentSpawnCount = FIRST_WAVE_COUNT;
-	public float gamerScore;
-
-	public Text scoreText;
-
+	
 	// Use this for initialization
 	void Start () {
 		waveSpawnCount = new List<int>();
 		waveSpawnCount.Add(FIRST_WAVE_COUNT);
-		resetInterWaveTimer();
-		resetIntraSpawnTimer();
-		gamerScore = 0;
-
+		interWaveTimer = 5.0f;
+		resetIntraWalkerSpawnTimer();
+		resetIntraBomberSpawnTimer();
+		totalGameScore = 0;
 	}
 
 	void resetInterWaveTimer(){
-		interWaveTimer = 5.0f; // sort of a constant
+		interWaveTimer = 10.0f; // sort of a constant
+		print ("Wave " + waveSpawnCount.Count + " Starting!");
 	}
 	
-	void resetIntraSpawnTimer(){
+	void resetIntraWalkerSpawnTimer(){
 		currentSpawnCount--;
-		intraSpawnTimer = Random.Range(1.0F, 3.0F);
+		intraWalkerSpawnTimer = Random.Range(1.0F, 3.0F);
+	}
+
+	void resetIntraBomberSpawnTimer(){
+		currentSpawnCount--;
+		intraBomberSpawnTimer = Random.Range(3.0F, 5.0F);
 	}
 
 	// Update is called once per frame
@@ -53,27 +63,48 @@ public class GameManager : MonoBehaviour {
 				resetInterWaveTimer();
 			}
 		} else { // Spawning
-			if(intraSpawnTimer > 0){
-				intraSpawnTimer -= Time.deltaTime;
-			} else {
-				GameObject walk = (GameObject)Instantiate (enemy, spawn.transform.position, rot);
-				walkers.Add(walk);
-				resetIntraSpawnTimer();
-				if(currentSpawnCount <= 0){
-					if(spawnCountLimit == FIRST_WAVE_COUNT){
-						spawnCountLimit = FIRST_WAVE_COUNT * 2;
-						waveSpawnCount.Add(spawnCountLimit);
-					} else {
-						spawnCountLimit = waveSpawnCount[waveSpawnCount.Count - 1] + waveSpawnCount[waveSpawnCount.Count - 2];
-						waveSpawnCount.Add(spawnCountLimit);
-					}
 
-					currentSpawnCount = spawnCountLimit;
-					waitForWave = true;
+			// Check for walker spawn
+			if(intraWalkerSpawnTimer > 0){
+				intraWalkerSpawnTimer -= Time.deltaTime;
+			} else {
+				GameObject walk = (GameObject)Instantiate (walkerPrefab, walkerSpawn.transform.position, rot);
+				walkers.Add(walk);
+				resetIntraWalkerSpawnTimer();
+				// Check for end wave, break out of function and skip bomber check
+				if(checkForEndWave()){
+					return;
 				}
+			}
+
+			// Check for bomber spawn
+			if(intraBomberSpawnTimer > 0){
+				intraBomberSpawnTimer -= Time.deltaTime;
+			} else {
+				GameObject bomb = (GameObject)Instantiate (bomberPrefab, bomberSpawn.transform.position, rot);
+				bombers.Add(bomb);
+				resetIntraBomberSpawnTimer();
+				checkForEndWave();
 			}
 		}
 	}
 
+		bool checkForEndWave(){
+			if(currentSpawnCount <= 0){
+				if(spawnCountLimit == FIRST_WAVE_COUNT){
+					spawnCountLimit = FIRST_WAVE_COUNT * 2;
+					waveSpawnCount.Add(spawnCountLimit);
+				} else {
+					spawnCountLimit = waveSpawnCount[waveSpawnCount.Count - 1] + waveSpawnCount[waveSpawnCount.Count - 2];
+					waveSpawnCount.Add(spawnCountLimit);
+				}
+				
+				currentSpawnCount = spawnCountLimit;
+				
+				waitForWave = true;
+				return true;
+			}
 
+			return false;
+		}
 }
