4f8404144476d40d96555a94c4164cf0487eed76
ToddWilliams459 <tkw8341@rit.edu>
2016-05-13 03:49:39 -0400

    Wizard + Fireball early stage

---
+++ Assets/Scripts/Wizard.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wizard : MonoBehaviour {
+
+	public GameObject fireballSpawn;
+	public GameObject fireballPrefab;
+	private Quaternion rot = Quaternion.Euler(0,0,0);
+
+
+	// Use this for initialization
+	void Start () {
+		conjure ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void conjure(){
+		GameObject ball = (GameObject)Instantiate (fireballPrefab, fireballSpawn.transform.position, rot);
+	}
+}
---
+++ Assets/Scripts/fireBall.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fireBall : MonoBehaviour {
+
+	public GameObject target;
+	GameManager gameManagerScript;
+
+	// Use this for initialization
+	void Start () {
+	gameManagerScript = FindObjectOfType (typeof(GameManager)) as GameManager;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position += new Vector3 (1,0,0) * Time.deltaTime;
+	//	this.transform.position = Vector3.MoveTowards(target.transform.position, target.transform.position,.001f)  * Time.deltaTime;
+	}
+}
