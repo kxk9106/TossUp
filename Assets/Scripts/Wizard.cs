614682ad2e257f733d0f7b84b90049e5886ee5bd
ToddWilliams459 <tkw8341@rit.edu>
2016-05-13 05:17:26 -0400

    Fireball almost fully functional
    Press space when there is a walker on the screen for results. Working on
    functionality plus slight bug.

--- Assets/Scripts/Wizard.cs
+++ Assets/Scripts/Wizard.cs
@@ -6,19 +6,42 @@ public class Wizard : MonoBehaviour {
 	public GameObject fireballSpawn;
 	public GameObject fireballPrefab;
 	private Quaternion rot = Quaternion.Euler(0,0,0);
+	GameManager gameManagerScript;
 
+
+	public GameObject target;
 
 	// Use this for initialization
 	void Start () {
-		conjure ();
+		gameManagerScript = FindObjectOfType (typeof(GameManager)) as GameManager;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			conjure ();
+		}
+		
 	}
 
 	void conjure(){
-		GameObject ball = (GameObject)Instantiate (fireballPrefab, fireballSpawn.transform.position, rot);
+		int i = 0;
+		float closest = 0f;
+		foreach (GameObject walker in gameManagerScript.walkers) 
+		{
+			float temp = Vector3.Distance (this.transform.position, gameManagerScript.walkers [i].transform.position);
+
+			if (temp > closest || closest == null) {
+				closest = temp;
+				target = gameManagerScript.walkers [i];
+			}
+
+		
+			i++;
+		}
+		//Debug.Log (target);
+		GameObject ball = (GameObject)Instantiate (fireballPrefab, fireballSpawn.transform.position, rot) as GameObject;
+
 	}
 }
--- Assets/Scripts/fireBall.cs
+++ Assets/Scripts/fireBall.cs
@@ -3,17 +3,30 @@ using System.Collections;
 
 public class fireBall : MonoBehaviour {
 
-	public GameObject target;
-	GameManager gameManagerScript;
+	GameObject target;
+	float speed = 2.5f  * Time.deltaTime;
+	Wizard wizardScript;
 
 	// Use this for initialization
 	void Start () {
-	gameManagerScript = FindObjectOfType (typeof(GameManager)) as GameManager;
+		wizardScript = FindObjectOfType (typeof(Wizard)) as Wizard;
+		target = wizardScript.target;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		this.transform.position += new Vector3 (1,0,0) * Time.deltaTime;
-	//	this.transform.position = Vector3.MoveTowards(target.transform.position, target.transform.position,.001f)  * Time.deltaTime;
+		//this.transform.position += new Vector3 (1,0,0) * Time.deltaTime;
+		if (target != null) {
+			this.transform.position = Vector3.MoveTowards (this.transform.position, target.transform.position, speed);
+		} else {
+			Destroy (this.gameObject);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Enemy") {
+			Destroy (coll.gameObject);
+			Destroy (this.gameObject);
+		}
 	}
 }
